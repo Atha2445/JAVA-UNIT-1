e54f38a85b156ae74c82ebca1d66f7cc08d38931
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2024-04-24 01:44:45 +0530

    Create for loop in java.java

---
+++ for loop in java.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class ForTest{
+    public static void main(String[] args){
+    Scanner scanner= new Scanner(System.in);
+    
+    int x;
+    x=scanner.nextInt();
+    for(int i=0;i<10;i++){
+        System.out.println(x);
+        x++;
+        }
+    }
+}
