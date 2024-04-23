eb676e68695e68d4e108effd731cd09a23785471
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2024-04-23 22:49:14 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Example{
+    public static void main(String[] args){
+        int num;
+        num=100;
+        num*=2;
+        System.out.print("Answer is : ");
+        System.out.println(num);
+    }
+}
